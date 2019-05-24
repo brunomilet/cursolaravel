ec0fb35d93e24f2fa64da645fe570ce20361c45e
Bruno Milet <brunomgrj@gmail.com>
2019-05-23 23:26:17 -0300

    Segundo commit

---
+++ resources/views/sobre.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+@section('titulo', 'Laravel - Sobre');
+
+@section('conteudo')
+<h1>Sobre nós</h1>
+<p>Deserunt quem eiusmod est aute fabulas ubi ipsum legam o ab aliqua senserit,
+        singulis sint nisi aliquip export. Si ipsum ubi aute, iis est tamen laboris.
+        Nisi de ex noster litteris, elit exquisitaque ullamco quem senserit ex nostrud
+        philosophari ea officia do de quorum familiaritatem, multos ita fabulas iis et
+        in amet mentitum a aut ab quis incididunt, offendit esse senserit.Ad e magna
+        cillum legam. In illum enim export expetendis, nam eu coniunctione et officia
+        multos se senserit fidelissimae ut expetendis ne vidisse. E nulla distinguantur,
+        se incididunt ex consequat, summis incurreret se irure eram eu de quid est
+        veniam. An incididunt o laborum, si multos quem tempor officia
+</p>
+@endsection
